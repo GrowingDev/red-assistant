837117ca37651ccdb43e9d80d48798c77649603d
GrowingDev <andreas.benz.privat@icloud.com>
2021-05-29 16:32:46 +0200

    er

---
+++ src/store/modules/lines.js
@@ -0,0 +1,133 @@
+import {app} from '../../models/app'
+export const lines = {
+    namespaced: true,
+    state: () => ({
+        list: [
+            {
+                id: 1,
+                history:[
+                    {
+                        amount: 20,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 30,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 50,
+                        timestamp: ""
+                    },
+                    
+                ]
+            },
+            {
+                id: 2,
+                history:[
+                    {
+                        amount: 20,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 30,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 50,
+                        timestamp: ""
+                    },
+                    
+                ]
+            },
+            {
+                id: 3,
+                history:[
+                    {
+                        amount: 20,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 30,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 50,
+                        timestamp: ""
+                    },
+                    
+                ]
+            },
+            {
+                id: 4,
+                history:[
+                    {
+                        amount: 20,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 30,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 50,
+                        timestamp: ""
+                    },
+                    
+                ]
+            },
+            {
+                id: 5,
+                history:[
+                    {
+                        amount: 20,
+                        timestamp: "",
+                        
+                    },
+                    {
+                        amount: 30,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 50,
+                        timestamp: ""
+                    },
+                    
+                ]
+            },
+            {
+                id: 6,
+                history:[
+                    {
+                        amount: 20,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 30,
+                        timestamp: ""
+                    },
+                    {
+                        amount: 50,
+                        timestamp: ""
+                    },
+                    
+                ]
+            }
+        ]
+    }),
+
+    getters: {
+        list: state => state.list,
+    },
+    actions: {
+        showLine({ dispatch }, id) {
+            dispatch("fetchLines", id);
+        }
+    },
+    mutations: {
+        fetchLines(state, id) {
+           
+            console.log(id);
+        },
+
+    },
+}
